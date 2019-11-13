57f70bbf3888664c4a2bb08d11a8d03166271d98
rmiller1 <richard.e.miller11@gmail.com>
2019-11-12 16:54:05 -0800

    changes

--- youtube_interface.py
+++ youtube_interface.py
@@ -10,7 +10,3 @@ y = 0
 m_win = tk.Tk()
 m_win.geometry("500x500")
 
-file = None
-valid_box = tk.Text(m_win, height=2, width=50)
-valid_box.pack()
-valid_box.insert(tk.END, "Upload or start analysis please.")
